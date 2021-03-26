31c5ff910f66d362b137df60046845722f0f8efb
DANIEL15-1 <81401457+DANIEL15-1@users.noreply.github.com>
2021-03-25 22:11:09 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+
+const texto1 = document.getElementById("texto1");
+const texto2 = document.getElementById("texto2");
+const texto3 = document.getElementById("texto3");
+const texto4 = document.getElementById("texto4");
+
+
+function mostrarpersonales(){
+texto1.innerHTML="Luis Daniel De Leon Carballo";
+texto2.innerHTML="Edad: 24 años";
+texto3.innerHTML="Sexo: masculino";
+texto4.innerHTML="No. Carne: 9989-21-734";
+}
+function datosdelcolegio(){
+    texto1.innerHTML=" Colegio yulimay P.C";
+    texto2.innerHTML="Dirrecion: 12 Calle, Zona 1 Cdad. de Guatemala";
+    texto3.innerHTML="Vision: Seguir siendo la mejor Institución.";
+    texto4.innerHTML="Mision: Reflejar en cada estudiante el temor a Dios.";
+}
+function datosdelauniversidad(){
+    texto1.innerHTML="Universidad Mariano Galvez Central";
+    texto2.innerHTML="Dirrecion: 3a. 9-00 zona 2, Interior Finca El Zapote, Cdad. de Guatemala";
+    texto3.innerHTML="Vision: Seguir siendo la mejor Institución.";
+    texto4.innerHTML="Mision: Reflejar en cada estudiante el temor a Dios.";
+}
+function Hobbies (){
+texto1.innerHTML="Hacer Deportes";
+texto2.innerHTML="Tocar Bateria";
+texto3.innerHTML="Pasar Tiempo con mi familia";
+texto4.innerHTML="Ver Series de TV";
+}
